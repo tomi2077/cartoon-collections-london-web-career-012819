0c44d522409df33320fab5ea109f158a0c30a19a
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-19 21:01:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,20 +9,11 @@ def summon_captain_planet(calls)
   return call_array
 end
 
-def lon
-      return true
-    else
-      return false
-    end
-  end
-end
-
 def long_planeteer_calls(calls)
   calls.any? do |call|
     call.length > 4
   end
 end
-
 
 def find_the_cheese# code an argument here
   # the array below is here to help
