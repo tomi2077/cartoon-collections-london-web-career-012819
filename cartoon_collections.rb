9d228660db633d88e451951b6bae572c0de6090f
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-19 21:13:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   while i < cheese_types.length
     return cheese_types[cheese] if array.include?(cheese_types[cheese])
-  i += 1
+    i += 1
   end
 end
 
