1e1b3afa7737f0f39a0620ef69e78e971e714a38
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-19 20:33:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(calls_long)
-  calls.each { |call| if call.length > 4 }
+  calls.each { |call| call.length > 4 }
   return true 
 end
 
