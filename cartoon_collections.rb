55769c637f935ca14478e3b1d8b5e51cb80186c9
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-19 20:25:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,11 +2,14 @@ def roll_call_dwarves(names)
   names.each_with_index { |i,index| puts "#{index+1}. #{i}" }
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)
   # Your code here
 end
 
 a.collect { |x| x + "!" }         #=> ["a!", "b!", "c!", "d!"]
+
+summon_captain_planet(planeteer_calls)
+#=> ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 
 def long_planeteer_calls# code an argument here
   # Your code here
