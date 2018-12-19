afeb283b77fe0eeaf2b7969b667d225327681daf
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-19 21:09:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.include?("cheddar" || "gouda" || "camembert")
-  return cheese
+  return 
   end
 end
 
