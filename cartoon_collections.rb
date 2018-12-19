12d24f06c244ffef83c9589640c14a9773760946
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-19 20:24:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,6 +6,8 @@ def summon_captain_planet# code an argument here
   # Your code here
 end
 
+a.collect { |x| x + "!" }         #=> ["a!", "b!", "c!", "d!"]
+
 def long_planeteer_calls# code an argument here
   # Your code here
 end
