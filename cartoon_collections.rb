9f8b6a0dfe3bec165f26ed71055020dd0156a2c7
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-19 20:20:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,12 @@ def roll_call_dwarves# code an argument here
   # Your code here
 end
 
+1.  Doc
+> 2.  Dopey
+> 3.  Bashful
+> 4.  Grumpy
+
+
 def summon_captain_planet# code an argument here
   # Your code here
 end
@@ -14,3 +20,4 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
