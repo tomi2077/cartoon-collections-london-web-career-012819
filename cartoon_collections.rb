5e6968dbd7c95a00e6f5c388754deb4ae3e2d28a
Tominiyi Ajayi <tominiyiajayi@gmail.com>
2018-12-19 20:23:38 +0000

    Automatically backed up by Learn

---
+++ cartoon_collections.rb
@@ -0,0 +1,17 @@
+def roll_call_dwarves(names)
+  names.each_with_index { |i,index| puts "#{index+1}. #{i}"
+end
+
+def summon_captain_planet# code an argument here
+  # Your code here
+end
+
+def long_planeteer_calls# code an argument here
+  # Your code here
+end
+
+def find_the_cheese# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+end
+
